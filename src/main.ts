35b0077944e43c96334e7db369582bb996b30f21
lebrancconvas <lebrancconvas@gmail.com>
2023-09-09 11:59:26 +0700

    💡 chore: comment to divide phase

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { THREE, renderer, scene, mainCamera } from './renderer';
 import { config } from './config';
 
+
+// Init.
+
+
+
 // Render.
 renderer.setSize(config.width, config.height);
 renderer.render(scene, mainCamera);
